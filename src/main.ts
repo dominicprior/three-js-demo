5ed63f977fd75117e18c4503919b9304c6bb4959
Dominic Prior <dominicprior42@gmail.com>
2025-04-06 18:51:23 +0100

    Change the w and h to 400

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,13 @@ import * as THREE from 'three';
 import { vertexShader } from './vert';
 import { fragmentShader } from './frag';
 
+const w = 400; // window.innerWidth;
+const h = 400; // window.innerHeight;
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 90, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const camera = new THREE.PerspectiveCamera( 90, w / h, 0.1, 1000 );
 
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.setSize( w, h );
 document.body.appendChild( renderer.domElement );
 
 const geometry = new THREE.BoxGeometry( 1, 1, 1, 4, 4, 4 );
