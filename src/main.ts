b73a1a09d5dc5badfd7590ce180f8159aaac0f1e
Dominic Prior <dominicprior42@gmail.com>
2025-04-07 07:35:37 +0100

    Change camera.position.z to 1.00

--- src/main.ts
+++ src/main.ts
@@ -25,7 +25,7 @@ const material = new THREE.RawShaderMaterial( {
 const cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
 
-camera.position.z = 1.01;
+camera.position.z = 1.00;
 
 // renderer.setAnimationLoop( animate );
 // function animate() {
