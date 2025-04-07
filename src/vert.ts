3a4b9fd0cb464af1baef9eb295df8e8bbdaad868
Dominic Prior <dominicprior42@gmail.com>
2025-04-07 10:24:37 +0100

    Add the lens

--- src/vert.ts
+++ src/vert.ts
@@ -7,12 +7,13 @@ attribute vec3 normal;
 attribute vec2 uv;
 varying vec2 vUv;
 void main() {
-  vec4 p;
-  vUv = uv;
-  p = modelViewMatrix * vec4(position, 1.);
+  vec4 p = modelViewMatrix * vec4(position, 1.);  // e.g. the centre of the plane in front of us is (0,0,-1,1)
+  float d = length(p.xyz); // distance from the camera
+  p.z -= d;
   gl_Position = projectionMatrix * p;
   if (modelViewMatrix[3][2] == -1.0) {
     gl_Position.x *= 0.5;
   }
+  vUv = uv;
 }
 `;
