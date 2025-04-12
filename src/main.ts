dbac51049779ed12bbc554a06b197489e1695104
Dominic Prior <dominicprior42@gmail.com>
2025-04-12 10:04:02 +0100

    Try to import glTF model smallfoo.glb

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,22 @@
 import * as THREE from 'three';
 import { vertexShader } from './vert';
 import { fragmentShader } from './frag';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 
 const w = 400; // window.innerWidth;
 const h = 400; // window.innerHeight;
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 140, w / h, 0.1, 1000 );
+const camera = new THREE.PerspectiveCamera( 60, w / h, 0.1, 1000 );
+const light = new THREE.AmbientLight( 0x404040 ); // soft white light
+scene.add( light );
+
+const loader = new GLTFLoader();
+loader.load('models/smallfoo.glb', function (gltf) {
+  console.log(gltf);
+  const model = gltf.scene;
+  scene.add(model);
+  model.position.set(0, 0, 0);
+});
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( w, h );
@@ -25,7 +36,7 @@ geometry.translate(0, 0, 4);
 const cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
 
-camera.position.z = 1.00;
+camera.position.z = 10.00;
 
 // renderer.setAnimationLoop( animate );
 // function animate() {
