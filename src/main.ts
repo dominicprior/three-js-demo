fb8de1fe369446c3fe758c6f469cf4849446e774
Dominic Prior <dominicprior42@gmail.com>
2025-04-12 16:07:45 +0100

    Tidy

--- src/main.ts
+++ src/main.ts
@@ -6,35 +6,29 @@ import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 const w = 400; // window.innerWidth;
 const h = 400; // window.innerHeight;
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 60, w / h, 0.1, 1000 );
-const light = new THREE.AmbientLight( 0xffffff );
-scene.add( light );
+const camera = new THREE.PerspectiveCamera(60, w / h, 0.1, 1000);
+camera.position.z = 10.00;
+const light = new THREE.AmbientLight(0xffffff);
+scene.add(light);
 
 const loader = new GLTFLoader();
 const astronaut = await loader.loadAsync('./models/smallfoo.glb');
 scene.add(astronaut.scene);
 
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize( w, h );
-document.body.appendChild( renderer.domElement );
+renderer.setSize(w, h);
+document.body.appendChild(renderer.domElement);
 
-const geometry = new THREE.PlaneGeometry(6,6,12,12);
-const material = new THREE.RawShaderMaterial( {
+const planeGeometry = new THREE.PlaneGeometry(6,6,12,12);
+const planeMaterial = new THREE.RawShaderMaterial({
   uniforms: {},
   vertexShader: vertexShader,
   fragmentShader: fragmentShader,
   wireframe: true,
   side: THREE.DoubleSide,
 });
-geometry.translate(0, 0, 4);
 
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+const planeMesh = new THREE.Mesh(planeGeometry, planeMaterial);
+scene.add(planeMesh);
 
-camera.position.z = 10.00;
-
-// renderer.setAnimationLoop( animate );
-// function animate() {
-  // cube.rotation.y += 0.01;
-  renderer.render( scene, camera );
-// }
+renderer.render(scene, camera);
